685ebd4ecf49a07e1b539a9aac3c74d3d78d329a
owren <palanders@owren.xyz>
2020-09-27 22:14:31 +0200

    recursive maze, but failed

--- game_logic/Board.py
+++ game_logic/Board.py
@@ -9,6 +9,8 @@ import random
 from functions import *
 
 wall = '#'
+N, S, E, W = 1, 2, 3, 4
+HOR, VER = 0, 1
 
 class Board:
 
@@ -35,13 +37,13 @@ class Board:
         direction = random.randint(1, 4)
         y_coordinate = random_odd_number(1, self.board.shape[0] - 2)
         x_coordinate = random_odd_number(1, self.board.shape[1] - 2)
-        if direction == 1:
+        if direction == N:
             self.board[y_coordinate][0] = ' '
-        elif direction == 2:
+        elif direction == S:
             self.board[y_coordinate][self.board.shape[1] - 1] = ' '
-        elif direction == 3:
+        elif direction == W:
             self.board[0][x_coordinate] = ' '
-        else:
+        elif direction == E:
             self.board[self.board.shape[0] - 1][x_coordinate] = ' '
 
 
@@ -56,20 +58,36 @@ class Board:
         """
         print(x0,x1,y0,y1)
         orientation = decide_orientation(x1 - x0, y1 - y0)
-        if orientation == 0:
+        if orientation == HOR:
+            if y1 - y0 <= 2:
+                print('sup')
+                return
+                print('ups')
             print(orientation)
             line = random_even_number(y0, y1)
             self.set_line(y0, y1, orientation, line)
-        elif orientation == 1:
+            clear_screen()
+            self.display_board()
+            self.recursive_devide(x0, line, y0, y1)
+            self.recursive_devide(line, x1, y0, y1)
+        elif orientation == VER:
+            if x1 - x0 <= 2:
+                print('sup2')
+                return
+                print('ups2')
             print(orientation)
             line = random_even_number(x0, x1)
             self.set_line(x0, x1, orientation, line)
+            clear_screen()
+            self.display_board()
+            self.recursive_devide(x0, x1, y0, line)
+            self.recursive_devide(x0, x1, line, y1)
 
 
     def set_line(self, min, max, orientation, coordinate):
         opening = random_odd_number(min, max)
         # vertival
-        if orientation == 0:
+        if orientation == HOR:
             for cell in range(min, max + 1):
                 self.board[coordinate][cell] = '#'
             self.board[coordinate][opening] = ' '
