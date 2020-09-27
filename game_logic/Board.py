d356ecda579b274d529c7a2c70c15af03b209451
owren <palanders@owren.xyz>
2020-09-27 20:56:03 +0200

    recursive_devide method splits one time

--- game_logic/Board.py
+++ game_logic/Board.py
@@ -1,15 +1,25 @@
+################################################################################
+# Pål Anders Wangen Owren                                                      #
+# Student nummer: 333704                                                       #
+################################################################################
+
 import numpy as np
 import random
+
+from functions import *
 
 wall = '#'
 
 class Board:
 
+
     def __init__(self, horizontal, vertical):
-        # creates the board - times two to make space for boarder inside, and pluss two boarder outside
-        self.board = np.full((horizontal * 2 + 2, vertical * 2 + 2), ' ')
+        # creates the board - times two to make space for boarder inside, minus one to get a odd number and pluss two boarder outside
+        self.board = np.full((horizontal * 2 - 1 + 2, vertical * 2 - 1 + 2), ' ')
         self.set_boarder()
         self.set_exit_point()
+        self.recursive_devide(1, self.board.shape[1] - 2, 1, self.board.shape[0] - 2)
+
 
     def set_boarder(self):
         # sets boarder around
@@ -20,10 +30,11 @@ class Board:
             self.board[0][col] = wall
             self.board[self.board.shape[0] - 1][col] = wall
 
+
     def set_exit_point(self):
         direction = random.randint(1, 4)
-        y_coordinate = random.randint(1, self.board.shape[0] - 2)
-        x_coordinate = random.randint(1, self.board.shape[1] - 2)
+        y_coordinate = random_odd_number(1, self.board.shape[0] - 2)
+        x_coordinate = random_odd_number(1, self.board.shape[1] - 2)
         if direction == 1:
             self.board[y_coordinate][0] = ' '
         elif direction == 2:
@@ -33,13 +44,41 @@ class Board:
         else:
             self.board[self.board.shape[0] - 1][x_coordinate] = ' '
 
-    def decide_orientation(self, width, height):
-        if width < height:
-            return 'horizontal'
-        elif width > height:
-            return 'vertical'
+
+    def recursive_devide(self, x0, x1, y0, y1):
+        """
+        ----------
+        Parameters:
+            x0: int - start position for x direction
+            x1: int - end position for x direction
+            y0: int - start position for y direction
+            y1: int - end position for y direction
+        """
+        print(x0,x1,y0,y1)
+        orientation = decide_orientation(x1 - x0, y1 - y0)
+        if orientation == 0:
+            print(orientation)
+            line = random_even_number(y0, y1)
+            self.set_line(y0, y1, orientation, line)
+        elif orientation == 1:
+            print(orientation)
+            line = random_even_number(x0, x1)
+            self.set_line(x0, x1, orientation, line)
+
+
+    def set_line(self, min, max, orientation, coordinate):
+        opening = random_odd_number(min, max)
+        # vertival
+        if orientation == 0:
+            for cell in range(min, max + 1):
+                self.board[coordinate][cell] = '#'
+            self.board[coordinate][opening] = ' '
+        # horizontal
         else:
-            return 'horizontal' if random.randint(0, 1) == 0 else 'vertical'
+            for cell in range(min, max + 1):
+                self.board[cell][coordinate] = '#'
+            self.board[opening][coordinate] = ' '
+
 
     def display_board(self):
         for row in self.board:
@@ -47,5 +86,3 @@ class Board:
             for cell in row:
                 string += str(cell) + str(' ')
             print(string)
-
-    pass
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+################################################################################
+# Pål Anders Wangen Owren                                                      #
+# Student nummer: 333704                                                       #
+################################################################################
+
 from game_logic.Board import Board
+from robust_IO import input_int
 
 
 def main():
+
     a = Board(5, 5)
     a.display_board()
 
